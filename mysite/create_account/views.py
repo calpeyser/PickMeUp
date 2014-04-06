3c0dabc9d146ad6d9a2b341fd09722958994586d
schwartzjj <jschwartz1992@gmail.com>
2014-04-06 17:54:11 -0400

    first commit

--- mysite/create_account/views.py
+++ mysite/create_account/views.py
@@ -1,3 +1,4 @@
+import CASClient, os
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from create_account.forms import UserForm, RideForm
@@ -32,3 +33,7 @@ def ride(request):
         'form': ride_form,
     })
 
+def authenticate(request):
+	C = CASClient.CASClient()
+	netid = C.Authenticate()
+	print "Hello %s" % netid
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from create_account.views import user
+import CASClient
 
 from django.contrib import admin
 admin.autodiscover()
@@ -10,5 +11,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^create_account/', 'create_account.views.user')
+    url(r'^create_account/', 'create_account.views.user'),
+    url(r'^$', 'create_account.views.authenticate')
 )
